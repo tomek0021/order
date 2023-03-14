4fcdeb6d4ba2c113aab0cd5e8e0ea5c085c2f63b
Tomek <trojek21@gmail.com>
2023-03-14 14:26:46 +0100

    gradle init

---
+++ lib/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `java`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
